5a2ac354580b03486fa818bc82eb596285da865d
Carl Oscar Aaro <hello@carloscar.com>
2021-03-15 00:07:25 +0100

    Added test cases for full coverage

--- tests/test_now.py
+++ tests/test_now.py
@@ -93,3 +93,103 @@ def test_now_list_datetime() -> None:
     assert list_a is not list_d
     assert list_b is not list_d
     assert list_c is not list_d
+
+
+def test_utcnow_now_functionality() -> None:
+    import utcnow
+    from utcnow import now, utcnow_
+
+    assert type(utcnow.now) is not str
+    assert not isinstance(utcnow.now, str)
+    assert type(utcnow.utcnow.now) is not str
+    assert not isinstance(utcnow.utcnow.now, str)
+    assert type(utcnow_.now) is not str
+    assert not isinstance(utcnow_.now, str)
+    assert type(now) is not str
+    assert not isinstance(now, str)
+
+    assert type(utcnow.now()) is str
+    assert isinstance(utcnow.now(), str)
+    assert type(utcnow.utcnow.now()) is str
+    assert isinstance(utcnow.utcnow.now(), str)
+    assert type(utcnow_.now()) is str
+    assert isinstance(utcnow_.now(), str)
+    assert type(now()) is str
+    assert isinstance(now(), str)
+
+    assert len(str(utcnow.now)) == 27
+    assert len(repr(utcnow.now)) == 27
+    assert len(utcnow.now()) == 27
+    assert len(str(utcnow.utcnow.now)) == 27
+    assert len(repr(utcnow.utcnow.now)) == 27
+    assert len(utcnow.utcnow.now()) == 27
+    assert len(str(utcnow_.now)) == 27
+    assert len(repr(utcnow_.now)) == 27
+    assert len(utcnow_.now()) == 27
+    assert len(str(now)) == 27
+    assert len(repr(now)) == 27
+    assert len(now()) == 27
+
+    a = utcnow.now()
+    b = utcnow.now()
+    c = utcnow.now()
+    d = utcnow.now()
+    e = utcnow.now()
+    f = utcnow.now()
+    assert a <= b <= c <= d <= e <= f
+
+    a = str(utcnow.now)
+    b = str(utcnow.now)
+    c = str(utcnow.now)
+    d = str(utcnow.now)
+    e = str(utcnow.now)
+    f = str(utcnow.now)
+    assert a <= b <= c <= d <= e <= f
+
+    a = utcnow.utcnow.now()
+    b = utcnow.utcnow.now()
+    c = utcnow.utcnow.now()
+    d = utcnow.utcnow.now()
+    e = utcnow.utcnow.now()
+    f = utcnow.utcnow.now()
+    assert a <= b <= c <= d <= e <= f
+
+    a = str(utcnow.utcnow.now)
+    b = str(utcnow.utcnow.now)
+    c = str(utcnow.utcnow.now)
+    d = str(utcnow.utcnow.now)
+    e = str(utcnow.utcnow.now)
+    f = str(utcnow.utcnow.now)
+    assert a <= b <= c <= d <= e <= f
+
+    a = utcnow_.now()
+    b = utcnow_.now()
+    c = utcnow_.now()
+    d = utcnow_.now()
+    e = utcnow_.now()
+    f = utcnow_.now()
+    assert a <= b <= c <= d <= e <= f
+
+    a = str(utcnow_.now)
+    b = str(utcnow_.now)
+    c = str(utcnow_.now)
+    d = str(utcnow_.now)
+    e = str(utcnow_.now)
+    f = str(utcnow_.now)
+    assert a <= b <= c <= d <= e <= f
+
+    a = now()
+    b = now()
+    c = now()
+    d = now()
+    e = now()
+    f = now()
+    assert a <= b <= c <= d <= e <= f
+
+    a = str(now)
+    b = str(now)
+    c = str(now)
+    d = str(now)
+    e = str(now)
+    f = str(now)
+    assert a <= b <= c <= d <= e <= f
